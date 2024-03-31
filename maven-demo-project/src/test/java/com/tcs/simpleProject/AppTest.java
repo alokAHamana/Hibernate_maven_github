d7e23a1383f25c437a2cc8063252a17adb489027
Alok <alokahamana@gmail.com>
2024-03-31 18:45:22 +0530

    Maven project commited to github

---
+++ maven-demo-project/src/test/java/com/tcs/simpleProject/AppTest.java
@@ -0,0 +1,16 @@
+package com.tcs.simpleProject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void test() {
+		
+		Assert.assertEquals("Hello Maven ", new String("Hello Maven "));
+		
+		
+	}
+
+}
